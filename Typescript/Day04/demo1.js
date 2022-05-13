d8b9ff6c697d20c68aa20c1dd132d28bbed0cdcd
Javabykiran-Angular <71135924+Javabykiran-Angular@users.noreply.github.com>
2022-05-13 09:41:50 +0530

    Day04

---
+++ Typescript/Day04/demo1.js
@@ -0,0 +1,19 @@
+//variable 
+var a1, a2, a$4;
+//Data Types
+// number(int,long,double,float,short),string(""/''),boolean(true/false), 
+// any(number,string,boolean), void (it is netural=> it does point any thing)
+// misec=> undefined (It is used for variable) & null(That value is used for Object)
+var a = 10; //Forward declaration & defination
+// console.log('Value of a is '+a);
+var b;
+// console.log("Value of b is "+b)
+var c;
+// console.log(`Value of C is ${c}`);
+var c1;
+c1 = 2.5;
+console.log('Value of c1 is ' + c1);
+c1 = 'Sumit';
+console.log('Value of c1 is ' + c1);
+c1 = true;
+console.log('Value of c1 is ' + c1);
---
+++ Typescript/Day04/demo1.ts
@@ -0,0 +1,61 @@
+
+//variable 
+var a1,a2,a$4;
+
+//Data Types
+// number(int,long,double,float,short),string(""/''),boolean(true/false), 
+// any(number,string,boolean), void (it is netural=> it does point any thing)
+// misec=> undefined (It is used for variable) & null(That value is used for Object)
+
+var a:number=10; //Forward declaration & defination
+// console.log('Value of a is '+a);
+var b:number;
+// console.log("Value of b is "+b)
+var c:string;
+// console.log(`Value of C is ${c}`);
+var c1:any;
+c1=2.5;
+console.log('Value of c1 is '+c1);
+c1='Sumit';
+console.log('Value of c1 is '+c1);
+c1=true;
+// console.log('Value of c1 is '+c1);
+var c2;
+//
+/**/
+
+//Type Assertion => any/Object
+
+var d:any;
+
+//2 ways
+//1 Angle bracket 
+//2 as syntax
+
+//1 Angle bracket
+
+var temp=(<string> d);
+// temp.
+
+//2 as syntax
+
+var temp2=(d as string);
+// temp2.
+
+// var f g;
+
+//Operator
+// Airthematic =>+,-,/,*,%
+// Logical=> &&,||
+// Relational => >,<,>=.<=, !=,==(it checks only value), ===(Strongly equality it checks type as well as value)
+// Unaray => ++,-- , pre/post , incement/decrement
+// Ternary => condition?expression1:expression2
+// assignment => =,+=,-=,/=,*=
+// bitwise => &,|,^,~,!,>>,<<
+
+
+
+
+
+
+
