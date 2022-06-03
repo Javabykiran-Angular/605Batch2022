a0ff8ed1871a847f563b64bdff82aadd1f463e61
Javabykiran-Angular <71135924+Javabykiran-Angular@users.noreply.github.com>
2022-06-03 11:01:07 +0530

    Day06

---
+++ Angular/Day06/event-binding3/event-binding3.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-event-binding3',
+  templateUrl: './event-binding3.component.html',
+  styleUrls: ['./event-binding3.component.css']
+})
+export class EventBinding3Component implements OnInit {
+
+  result:number=0;
+  name:string='';
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onAddition(num1,num2){
+
+      let n1=+num1;
+      let n2=+num2;
+      // this.result=num1+num2;
+      this.result=n1+n2;
+  }
+
+  onChange(myname){
+    console.log('Change event Occur.....')
+    myname.style.background='green';
+    myname.style.color='white';
+  }
+
+  onkeyUp(){
+    console.log("Key Up event Occur....")
+  }
+
+  onkeyDown(){
+    console.log('Key Down event occur..')
+  }
+
+ 
+
+}
